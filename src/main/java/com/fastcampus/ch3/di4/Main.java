fb2311aab6589b96a3f29921746bb06e287d685e
SonYJ <soja0529@gmail.com>
2025-01-30 13:19:52 +0900

    3-7 완

---
+++ src/main/java/com/fastcampus/ch3/di4/Main.java
@@ -0,0 +1,35 @@
+package com.fastcampus.ch3.di4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+//@SpringBootApplication        // SBP는 아래 3개의 애너테이션을 같이 사용하는 것과 동일함
+// @SpringBootConfiguration        // @Confiquration과 동일하다
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext ac = SpringApplication.run(Main.class, args);
+//        System.out.println("ac = " + ac);
+
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+
+        Arrays.sort(beanDefinitionNames);       // 빈 목록이 담긴 배열을 정렬
+        Arrays.stream(beanDefinitionNames)      // 배열을 스트림으로 변환
+                .filter(b->!b.startsWith("org"))    // org로 시작하는 빈의 이름을 제외
+                .forEach(System.out::println);  // 스트림 요소를 하나씩 꺼내서 출력
+    }
+
+//    @Bean
+//    MyBean myBean() { return new MyBean(); }
+}
---
+++ src/main/java/com/fastcampus/ch3/di4/MyBean.java
@@ -0,0 +1,7 @@
+package com.fastcampus.ch3.di4;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyBean {
+}
